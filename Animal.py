f99578d865bea160b150bfdd80b4dd4a04f534e5
Ryan <ryancheung@MacwBook-Air-7.local>
2025-02-18 19:48:07 -0500

    Changed Animal class and usage with extra functions

--- Animal.py
+++ Animal.py
@@ -1,7 +1,20 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name): 
+        self.__name = name
+        print("hello i am", self.__name)
 
     def talk(self):
         print("hi")
+
+    def walk(self):
+        print("*steps forward*")
+
+    def eat(self):
+        print("*chomp*")
+
+    def jump(self):
+        print("*boing*")
+
+    def poop(self):
+        print("bowel emptied")
 
