3fa3116e0332c6aab2ad1555ed061052d1a39277
Ryan <ryancheung@MacwBook-Air-7.local>
2025-02-21 21:52:03 -0500

    Added additional functions to the animal class

--- Animal.py
+++ Animal.py
@@ -1,10 +1,11 @@
 class Animal:
-    def __init__(self, name, hungry = True, thirsty = True, needtopoop = True, dirty = True): 
+    def __init__(self, name, hungry = True, thirsty = True, needtopoop = True, dirty = True, walked = False): 
         self.__name = name
         self.__hungry = hungry
         self.__thristy = thirsty
         self.__needtopoop = needtopoop
         self.__dirty = dirty
+        self.__walks = walked
         print("hello i am", self.__name)
         print("I want to go on a walk. I am hungry. I want to jump. I need to use the bathroom")
 
@@ -13,6 +14,7 @@ class Animal:
 
     def walk(self):
         print("*steps forward*")
+        self.__walks = True
 
     def eat(self):
         self.__hungry = False
@@ -31,4 +33,4 @@ class Animal:
         print("All clean now")
 
     def status(self):
-        print(self.__name, f''' is hungry? {self.__hungry}. is thirsty? {self.__thristy}. needs to poop? {self.__needtopoop}. is dirty? {self.__dirty}''')
+        print(self.__name, f' is hungry? {self.__hungry}. is thirsty? {self.__thristy}. needs to poop? {self.__needtopoop}. is dirty? {self.__dirty}. Has been walked? {self.__walks}.')
